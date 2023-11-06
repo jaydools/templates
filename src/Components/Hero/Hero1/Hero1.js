e502458448904b0f6f3c3b1f5a4a57e631ae87bc
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-06 18:40:46 -0500

    started first mobile hero layout
    switching to hero branch to continue

--- src/Components/Hero/Hero1/Hero1.js
+++ src/Components/Hero/Hero1/Hero1.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import placeholder from "../../../Assets/placeholder.jpg"
+import "./Hero1.scss"
 
 function Hero1() {
-  return (
-    <div>Hero1</div>
-  )
+    return (
+        <main>
+            <div className='hero-container'>
+                <div className='leftside'>
+                    <h1 className='leftside__header'>Lorem ipsum dolor sit amet </h1>
+                    <h3 className='leftside__body'>Urabitur venenatis pulvinar lorem in fermentum. Sed vitae erat 
+                        ut sapien mollis ultricies. 
+                    </h3>
+                    <div className='leftside__button'>
+                        <button className='leftside__button--click'>Call To Action</button>
+                    </div>
+
+                </div>
+                <div className='rightside'>
+                        <img className='rightside__img' src={placeholder} alt="placeholder img" />
+                </div>
+
+            </div>
+        </main>
+    )
 }
 
 export default Hero1
