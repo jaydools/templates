e9794899c51115bfcd61a19aa1ad7aba81a66260
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-05 22:51:35 -0500

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.scss"
+
+function Header() {
+
+    // Logic up here 
+
+  return (
+    <div>
+        maybe use state and call an array of components 
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Hero.scss"
+
+function Hero() {
+  
+    // Logic up here 
+
+  return (
+    <div>
+        figure out how to make a slideshow and then start making templates for hero banners
+    </div>
+  )
+}
+
+export default Hero
