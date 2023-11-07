2667c14877c3ec1d1a59fe5a86ed5bf726588c9d
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-06 19:05:34 -0500

    hero2 complete
    fix hero 1 for breakpoints and build hero3

--- src/Components/Hero/Hero2/Hero2.js
+++ src/Components/Hero/Hero2/Hero2.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import placeholder from "../../../Assets/placeholder.jpg"
+import "./Hero2.scss"
 
 function Hero2() {
-  return (
-    <div>Hero2</div>
-  )
+    return (
+        <main>
+            <div className='hero-container'>
+
+            <div className='leftside'>
+                        <img className='leftside__img' src={placeholder} alt="placeholder img" />
+                </div>
+
+                <div className='rightside'>
+                    <h1 className='rightside__header'>Lorem ipsum dolor sit amet </h1>
+                    <h3 className='rightside__body'>Urabitur venenatis pulvinar lorem in fermentum. Sed vitae erat 
+                        ut sapien mollis ultricies. 
+                    </h3>
+                    <div className='rightside__button'>
+                        <button className='rightside__button--click'>Call To Action</button>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </main>
+    )
 }
 
 export default Hero2
